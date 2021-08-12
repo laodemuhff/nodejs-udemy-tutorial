485ede28d3c1ba2198c8bec37c92403ba339922d
tpid-farhan <farhan@technopartner.id>
2021-08-12 23:02:37 +0700

    [mod] more about string

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@ let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 4;
 
-let calculationDescription = '('+defaultResult+' + 10) * 4';
+let calculationDescription = `(${defaultResult} + 10) * 4`;
+let errorMessage = 'And Error \n' + 'Has Occured!';
 
-outputResult(currentResult, calculationDescription)
+outputResult(currentResult, errorMessage)
