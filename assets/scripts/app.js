ea4613fb71d4abf6bf4b21a8f7fa2ec79f93cf6b
tpid-farhan <farhan@technopartner.id>
2021-08-12 23:03:23 +0700

    [add] example adding numbers or string

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,4 +7,7 @@ currentResult = (currentResult + 10) * 4;
 let calculationDescription = `(${defaultResult} + 10) * 4`;
 let errorMessage = 'And Error \n' + 'Has Occured!';
 
+console.log(30 + '30'); //3030
+console.log(30 + 30); //60
+
 outputResult(currentResult, errorMessage)
