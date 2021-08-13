e0e0a5752cc798b4b3f11cad7a24ed3bf16d4ba3
tpid-farhan <farhan@technopartner.id>
2021-08-13 21:43:24 +0700

    shadowed variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,3 +14,12 @@ let calculationDescription = `(${currentResult} + 10) * 4`;
 currentResult = (currentResult + 10) * 4; 
 
 outputResult(currentResult, calculationDescription)
+
+// shadowed variables
+let userName = "Farhan";
+function greetUser(name){
+    let userName = name
+    alert(userName);
+}
+userName = "Manu"
+greetUser('Max');
