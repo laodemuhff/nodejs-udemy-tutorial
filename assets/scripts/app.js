5c945ae8491718269fd865c5007467840d7f5264
tpid-farhan <farhan@technopartner.id>
2021-08-13 22:11:54 +0700

    init

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2){
-    const result = num1 + num2;
+function add(){
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
+}  
 
-    return result;
-}   
-
-currentResult = add(10,10);
-
-let calculationDescription = `(${currentResult} + 10) * 4`;
-
-currentResult = (currentResult + 10) * 4; 
-
-outputResult(currentResult, calculationDescription)
-
-// shadowed variables
-let userName = "Farhan";
-function greetUser(name){
-    let userName = name
-    alert(userName);
-}
-userName = "Manu"
-greetUser('Max');
+addBtn.addEventListener('click', add)
