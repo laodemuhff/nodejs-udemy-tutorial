21650cf75a024b513394284126a4b828b93af04f
tpid-farhan <farhan@technopartner.id>
2021-08-13 19:14:37 +0700

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,16 @@
 const defaultResult = 0;
-
 let currentResult = defaultResult;
+
+function add(num1, num2){
+    const result = num1 + num2;
+    alert('The Result is '+result);
+}   
+
+add(10,10);
+add(5,5);
 
 currentResult = (currentResult + 10) * 4;
 
 let calculationDescription = `(${defaultResult} + 10) * 4`;
-let errorMessage = 'And Error \n' + 'Has Occured!';
-
-console.log(30 + '30'); //3030
-console.log(30 + 30); //60
 
 outputResult(currentResult, errorMessage)
