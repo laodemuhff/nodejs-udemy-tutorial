95329d249f3f7abe8820a5a2accab4d13c7976de
tpid-farhan <farhan@technopartner.id>
2021-08-13 19:22:22 +0700

    bugfix

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,14 +3,14 @@ let currentResult = defaultResult;
 
 function add(num1, num2){
     const result = num1 + num2;
-    alert('The Result is '+result);
+
+    return result;
 }   
 
-add(10,10);
-add(5,5);
+currentResult = add(10,10);
 
-currentResult = (currentResult + 10) * 4;
+currentResult = (currentResult + 10) * 4; 
 
 let calculationDescription = `(${defaultResult} + 10) * 4`;
 
-outputResult(currentResult, errorMessage)
+outputResult(currentResult, calculationDescription)
