23f8ed52e14c24c59931e0f356b27e638f2857e0
tpid-farhan <farhan@technopartner.id>
2021-08-14 15:39:36 +0700

    more operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,28 +15,29 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
 function add(){
     const enteredNumber = getUserInput()
     const initialResult = currentResult
-    currentResult = currentResult + parseInt(enteredNumber);
+    currentResult += parseInt(enteredNumber);
+    // alert(++currentResult)
     createAndWriteOutput('+', initialResult, enteredNumber);
 }  
 
 function substract(){
     const enteredNumber = getUserInput()
     const initialResult = currentResult
-    currentResult = currentResult - parseInt(enteredNumber);
+    currentResult -= parseInt(enteredNumber);
     createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
 function multiply(){
     const enteredNumber = getUserInput()
     const initialResult = currentResult
-    currentResult = currentResult * parseInt(enteredNumber);
+    currentResult *= parseInt(enteredNumber);
     createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
 function divide(){
     const enteredNumber = getUserInput()
     const initialResult = currentResult
-    currentResult = currentResult / parseInt(enteredNumber);
+    currentResult /= parseInt(enteredNumber);
     createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
