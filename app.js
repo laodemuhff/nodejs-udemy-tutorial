000aa14ea1153583d290cee6fb66a552925b2b03
Farhan Fauzan AVO <farhan@AVOs-MacBook-Pro.local>
2021-09-06 23:15:25 +0700

    add

--- app.js
+++ app.js
@@ -1,103 +1,14 @@
-// let name = 'Max';
+const addListenerBtn = document.getElementById('add-listener-btn');
+const clickableBtn = document.getElementById('clickable-btn');
+const messageInput = document.getElementById('click-message-input');
 
-// if (name === 'Max') {
-//   let hobbies = ['Sports', 'Cooking'];
-//   console.log(hobbies);
-// }
-
-// function greet() {
-//   let age = 30;
-//   let name = 'Manuel';
-//   console.log(name, age, hobbies);
-// }
-
-// console.log(name, hobbies);
-
-// greet();
-
-
-// function test(){
-//   for (let index = 0; index < 5; index++) {
-//     for (let index = 0; index < 11; index++) {
-//       var element = index;
-//     }
-//   }
-//   console.log(element);
-// }
-
-// test();
-// console.log(element);
-
-// var name;
-// var name;
-
-// understanding hoisting
-// let userName;
-
-// console.log(userName);
-
-// strict mode 
-
-// "use strict"
-
-
-// function test(){
-//   while(true){
-//     let undefined = true;
-//     let userName = "Max"
-//     console.log(userName);
-//     break;
-//   }
-// }
-
-// function test2(){
-//   while(true){
-//     let userNames = "Farhan"
-//     console.log(userNames);
-//     break;
-//   }
-// }
-
-// test()
-// test2()
-
-// if(true){
-//   "use strict"
-//   userName = "Max"
-//   console.log(userName);
-// }
-
-// if(true){
-//   userNames = "Farhan"
-//   console.log(userNames);
-// }
-
-// "use strict"
-
-// var undefined = true;
-// let undefined = true;
-
-// userName = "Max" 
-// console.log(userName);
-
-// let undefined;
-// "use strict"
-// var undefined = true;
-
-
-
-// let undefined = true;
-// function test(){
-
-//   console.log(undefined);
-// }
-
-// test();
-
-"use strict"
-
-if(true){
-  let undefined = true;
-
-  console.log(undefined);
+function printMessage() {
+  const value = messageInput.value;
+  console.log(value || 'Clicked me!');
 }
+
+function addListener() {
+  clickableBtn.addEventListener('click', printMessage);
+}
+
+addListenerBtn.addEventListener('click', addListener);
