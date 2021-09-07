1910cea120a37d8cd79acf9acbad8493cba6a354
Farhan Fauzan AVO <farhan@AVOs-MacBook-Pro.local>
2021-09-07 11:05:13 +0700

    test

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-const addListenerBtn = document.getElementById('add-listener-btn');
-const clickableBtn = document.getElementById('clickable-btn');
-const messageInput = document.getElementById('click-message-input');
-
-function printMessage() {
-  const value = messageInput.value;
-  console.log(value || 'Clicked me!');
-}
-
-function addListener() {
-  clickableBtn.addEventListener('click', () => {
-    const value = messageInput.value;
-    console.log(value || 'Clicked me!');
-  });
-}
-
-addListenerBtn.addEventListener('click', addListener);
+const startGameBtn = document.getElementById('start-game-btn');
