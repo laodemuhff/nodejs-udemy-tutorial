20dc03a6c3195f7f84f2f5f1cb8314612b6dc67b
Farhan Fauzan AVO <farhan@AVOs-MacBook-Pro.local>
2021-09-13 08:06:32 +0700

    tst

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,359 +1,56 @@
-const BASIC_ATTACK = 'ATTACK';
-const STRONG_ATTACK = 'STRONG ATTACK';
-const LOG_EVENT_HERO_BASIC_ATTACK = 'LOG EVENT HERO BASIC ATTACK';
-const LOG_EVENT_HERO_STRONG_ATTACK = 'LOG EVENT HERO STRONG ATTACK';
-const LOG_EVENT_MONSTER_ATTACK = 'LOG EVENT MONSTER ATTACK';
-const LOG_EVENT_HERO_HEAL = 'LOG EVENT HERO HEAL';
-const LOG_EVENT_HERO_UNDO_DEATH = 'LOG EVENT HERO UNDO DEATH';
-const log = [];
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-const hero = {
-    name: "Goku",
-    maximumAttackPower: 30,
-    maximumStrongAttackPower: 60,
-    maximumHeal: 10
-}
-const monster = {
-    name: "Cell",
-    maximumAttackPower: 50,
-    maximumHeal: 15
+// Gets input from input field
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
 }
 
-let isFinish;
-let hasBonusLife;
-let lastLogEntry;
-
-init();
-
-function init() {
-
-    isFinish = false;
-    hasBonusLife = true;
-
-    const maximumLife = getMaximumLife();
-
-    monster.currentLifeBar = maximumLife;
-    hero.currentLifeBar = maximumLife;
-
-    adjustHealthBars(maximumLife);
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor file
 }
 
-function getMaximumLife() {
-    const enteredNumber = prompt('Enter Maximum Value : ', 100);
-
-    const parsedNumber = parseInt(enteredNumber);
-    console.log(parsedNumber);
-
-    if (isNaN(parsedNumber) || parsedNumber < 0) {
-        throw { message: 'Invalid user input, not a number !' };
-    }
-
-    return parsedNumber;
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
-function restartGame() {
-    init();
-    resetState(hero_sprites, 1000)
-    resetState(monster_sprites, 1000)
-
-    addBonusLife()
-    enabledButton();
+function calculate(operation) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  let operator;
+  if (operation === 'ADD') {
+    currentResult += enteredNumber;
+    operator = '+';
+  } else if (operation === 'SUBTRACT') {
+    currentResult -= enteredNumber;
+    operator = '-';
+  } else if (operation === 'MULTIPLY') {
+    currentResult *= enteredNumber;
+    operator = '*';
+  } else {
+    currentResult /= enteredNumber;
+    operator = '/';
+  }
+  createAndWriteOutput(operator, initialResult, enteredNumber);
+  writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
-function resetState(sprites, timeout) {
-    // stand again after doing action
-    setTimeout(() => {
-        sprites.standing()
-    }, timeout);
-}
-
-function cekWin() {
-    // cek the result
-    if (monster.currentLifeBar <= 0 && hero.currentLifeBar > 0) {
-        monster_sprites.dead()
-        setTimeout(() => {
-            if (confirm(`You've Win !, Restart the Game ?`)) {
-                restartGame()
-            }
-        }, 1300);
-        isFinish = true;
-
-    } else if (monster.currentLifeBar > 0 && hero.currentLifeBar <= 0) {
-        hero_sprites.dead()
-
-        if (hasBonusLife) {
-            setTimeout(() => {
-                if (confirm(`You're dead by now, but you still have 1 bonus life, are you want to use it?`)) {
-                    hasBonusLife = false;
-
-                    removeBonusLife();
-                    undoDeath();
-                } else {
-                    if (confirm(`You've Lost !, Restart the Game ?`)) {
-                        restartGame()
-                    }
-                }
-            }, 1300)
-        } else {
-            setTimeout(() => {
-                if (confirm(`You've Lost !, Restart the Game ?`)) {
-                    restartGame()
-                }
-            }, 1300);
-            isFinish = true;
-        }
-
-    } else if (monster.currentLifeBar <= 0 && hero.currentLifeBar <= 0) {
-        if (confirm(`It's a Draw !, Restart the Game ?`)) {
-            restartGame()
-        }
-        isFinish = true;
-    }
-}
-
-function setModeAttack(mode) {
-    let maxAttack;
-    let logAttack;
-    const prevLifeBar = monster.currentLifeBar;
-
-    maxAttack = mode === BASIC_ATTACK ?
-        hero.maximumAttackPower :
-        hero.maximumStrongAttackPower;
-
-    logAttack = mode === BASIC_ATTACK ?
-        LOG_EVENT_HERO_BASIC_ATTACK :
-        LOG_EVENT_HERO_STRONG_ATTACK;
-
-    disabledButton()
-
-    // animate punch
-    hero_sprites.punch()
-    resetState(hero_sprites, 800)
-
-    const demageToMonster = dealMonsterDamage(maxAttack);
-    monster.currentLifeBar -= demageToMonster;
-    cekWin()
-
-    if (!isFinish) {
-        monsterResponse()
-    }
-
-    writeToLog({
-        event: logAttack,
-        target: 'MONSTER',
-        dealtDemage: demageToMonster,
-        prevTargetHealth: prevLifeBar,
-        currentTargetHealth: monster.currentLifeBar
-    });
-}
-
-function changeBtnProperties(btn, isDisabled) {
-    if (isDisabled) {
-        btn.disabled = true;
-        btn.style.background = "grey";
-    } else {
-        btn.disabled = false;
-        btn.style.background = "#ff0062";
-    }
-}
-
-function disabledButton() {
-    changeBtnProperties(attackBtn, true)
-    changeBtnProperties(strongAttackBtn, true)
-    changeBtnProperties(healBtn, true)
-    changeBtnProperties(logBtn, true)
-}
-
-function enabledButton() {
-    setTimeout(() => {
-        changeBtnProperties(attackBtn, false)
-        changeBtnProperties(strongAttackBtn, false)
-        changeBtnProperties(healBtn, false)
-        changeBtnProperties(logBtn, false)
-    }, 1000);
-}
-
-function monsterResponse() {
-    // wait for 2.5 second for monster to respond
-    setTimeout(function () {
-        const prevLifeBar = hero.currentLifeBar;
-
-        monster_sprites.kick()
-        resetState(monster_sprites, 1200)
-
-        const demageToHero = dealPlayerDamage(monster.maximumAttackPower);
-        hero.currentLifeBar -= demageToHero;
-        cekWin()
-
-        enabledButton();
-
-        writeToLog({
-            event: LOG_EVENT_MONSTER_ATTACK,
-            target: 'HERO',
-            dealtDemage: demageToHero,
-            prevTargetHealth: prevLifeBar,
-            currentTargetHealth: hero.currentLifeBar
-        });
-    }, 2500);
-}
-
-function attack() {
-    setModeAttack(BASIC_ATTACK);
-}
-
-function strongAttack() {
-    setModeAttack(STRONG_ATTACK);
-}
-
-function heal() {
-    const prevLifeBar = hero.currentLifeBar;
-
-    disabledButton();
-
-    // animate heal
-    hero_sprites.heal()
-    resetState(hero_sprites, 1500)
-
-    const healValue = increasePlayerHealth(hero.maximumHeal)
-    if (hero.currentLifeBar + healValue > maximumLife) {
-        hero.currentLifeBar = maximumLife
-    } else {
-        hero.currentLifeBar += healValue
-    }
-
-    cekWin()
-
-    if (!isFinish) {
-        monsterResponse()
-    }
-
-    writeToLog({
-        event: LOG_EVENT_HERO_HEAL,
-        target: 'HERO',
-        addedHealth: healValue,
-        prevTargetHealth: prevLifeBar,
-        currentTargetHealth: hero.currentLifeBar
-    });
-}
-
-function undoDeath() {
-    const prevLifeBar = hero.currentLifeBar;
-
-    disabledButton();
-
-    // animate heal
-    hero_sprites.heal()
-    resetState(hero_sprites, 1500)
-
-    undoPlayerHealthAfterDeath(hero.maximumHeal)
-    hero.currentLifeBar = 0 + hero.maximumHeal
-
-    monsterResponse()
-
-    writeToLog({
-        event: LOG_EVENT_HERO_UNDO_DEATH,
-        target: 'HERO',
-        addedHealth: hero.maximumHeal,
-        prevTargetHealth: prevLifeBar,
-        currentTargetHealth: hero.currentLifeBar
-    });
-}
-
-function writeToLog(data) {
-
-    const obj = {
-        event: data.event,
-        target: data.target,
-        prevTargetHealth: data.prevTargetHealth,
-        currentTargetHealth: data.currentTargetHealth
-    };
-
-    if (data.event === LOG_EVENT_HERO_BASIC_ATTACK ||
-        data.event === LOG_EVENT_HERO_STRONG_ATTACK ||
-        data.event === LOG_EVENT_MONSTER_ATTACK) {
-        obj.dealtDemage = data.dealtDemage ?? null;
-
-    } else if (data.event === LOG_EVENT_HERO_HEAL || data.event === LOG_EVENT_HERO_UNDO_DEATH) {
-        obj.addedHealth = data.addedHealth ?? null;
-    }
-
-    z
-    log.push(obj);
-}
-
-function showLog() {
-
-    // ==> only print index 0, baceuse after i becoming 1 it will never again reach i++ hence it will still skip until forof ends
-    // let i = 0;
-    // for (const lg of log) {
-    //     if(i === 1) continue;
-    //     console.log(`${i}`);
-    //     for (const key in lg) {
-    //         if (key === 'event') {
-    //             continue;
-    //         }
-    //         console.log(`${key} ==> ${lg[key]}`);
-    //     }
-    //     i++;
-    // }
-
-    // ==> everthing will be skipped except for console log "test" because it will never again reach i++ hence every index got skipped.
-    let i = 0;
-    for (const lg of log) {
-        console.log('test');
-        if (i === 0) continue;
-        console.log(`${i}`);
-        for (const key in lg) {
-            if (key === 'event') {
-                continue;
-            }
-            console.log(`${key} ==> ${lg[key]}`);
-        }
-        i++;
-    }
-
-
-    // ==> index 0 will skipped, the rest is still executed beacause i++ still gonna happen after every iteration
-    // for (let index = 0; index < log.length; index++) {
-    //     if(index == 0){
-    //         continue;
-    //     }
-
-    //     const element = log[index];
-    //     console.log(`#${index}`);
-    //     for(const key in element){
-    //         if(key === "event"){
-    //             continue;
-    //         }
-    //         console.log(`${key} => ${element[key]}`);
-    //     } 
-    // }
-
-    // ==>infinity loop (be carefull with while loop when using continue or break)
-    // let index = 0;
-    // do{
-    //     console.log('test');
-    //     if(index == 0){
-    //         continue;
-    //     }
-    //     console.log(`${index}`)
-    //     for (const key in log[index]) {
-    //         if (key == 'event') {
-    //             continue;
-    //         }
-    //         console.log(`${key} => ${log[index][key]}`);
-    //     }
-    //     index++;
-    // }while(index < log.length);
-
-    // you cant use break on foreach
-    // ketika mau merubah data/collection dalam jumlah yang banyak sebaiknya pakai forEach.
-    array.forEach(element => {
-        
-    });
-}
-
-attackBtn.addEventListener('click', attack)
-strongAttackBtn.addEventListener('click', strongAttack)
-healBtn.addEventListener('click', heal)
-logBtn.addEventListener('click', showLog)
+addBtn.addEventListener('click', calculate.bind(this, 'ADD'));
+subtractBtn.addEventListener('click', calculate.bind(this, 'SUBTRACT'));
+multiplyBtn.addEventListener('click', calculate.bind(this, 'MULTIPLY'));
+divideBtn.addEventListener('click', calculate.bind(this, 'DIVIDE'));
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,55 +1,13 @@
-const monsterHealthBar = document.getElementById('monster-health');
-const playerHealthBar = document.getElementById('player-health');
-// const bonusLifeEl = document.getElementById('bonus-life');
+const usrInput = document.getElementById('input-number');
+const addBtn = document.getElementById('btn-add');
+const subtractBtn = document.getElementById('btn-subtract');
+const multiplyBtn = document.getElementById('btn-multiply');
+const divideBtn = document.getElementById('btn-divide');
 
-const attackBtn = document.getElementById('attack-btn');
-const strongAttackBtn = document.getElementById('strong-attack-btn');
-const healBtn = document.getElementById('heal-btn');
-const logBtn = document.getElementById('log-btn');
+const currentResultOutput = document.getElementById('current-result');
+const currentCalculationOutput = document.getElementById('current-calculation');
 
-function adjustHealthBars(maxLife) {
-  monsterHealthBar.max = maxLife;
-  monsterHealthBar.value = maxLife;
-  playerHealthBar.max = maxLife;
-  playerHealthBar.value = maxLife;
-}
-
-function dealMonsterDamage(damage) {
-  const dealtDamage = Math.random() * damage;
-  monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
-  return dealtDamage;
-}
-
-function dealPlayerDamage(damage) {
-  const dealtDamage = Math.random() * damage;
-  playerHealthBar.value = +playerHealthBar.value - dealtDamage;
-  return dealtDamage;
-}
-
-function increasePlayerHealth(heal) {
-  const healValue = Math.random() * heal;
-  playerHealthBar.value = +playerHealthBar.value + healValue;
-  return healValue;
-}
-
-function undoPlayerHealthAfterDeath(heal){
-    playerHealthBar.value = 0 + heal;
-}
-
-function resetGame(value) {
-  playerHealthBar.value = value;
-  monsterHealthBar.value = value;
-}
-
-function removeBonusLife() {
-  const bonusLifeEl = document.getElementById('bonus-life');
-  bonusLifeEl.parentNode.removeChild(bonusLifeEl);
-}
-
-function addBonusLife() {
-  document.getElementById('player-health-bonus').innerHTML = 'PLAYER HEALTH<span id="bonus-life">1</span>'
-}
-
-function setPlayerHealth(health) {
-  playerHealthBar.value = health;
+function outputResult(result, text) {
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
 }
