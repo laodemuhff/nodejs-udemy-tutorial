2c03826e7966e462d24bb406a731fcc9d99f7f39
Farhan Fauzan AVO <farhan@AVOs-MacBook-Pro.local>
2021-09-13 08:02:22 +0700

    test

--- assignment.js
+++ assignment.js
@@ -1,5 +1,27 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
+const sayHello = name => console.log(`Hi ${name}`);
+const sayHello1 = (name, phrase = "Hello") => console.log(`${phrase} ${name}`)
+const sayHello2 = () => console.log(`Hi Fauzan`)
+const sayHello3 = (name, phrase = "Hello") => phrase + ' ' + name; 
+
+sayHello("Farhan");
+sayHello1("Dimas", "Hi");
+sayHello2()
+console.log(sayHello3("Aji"))
+
+const checkinput = (callback, ...data) => {
+    let found = false;
+    for(const item of data){
+        if(item === ''){
+            found = true
+        }
+    }
+    if(!found){
+        callback();
+    }
 }
 
-sayHello();
+const sayHi = () => {
+    alert('There is No Empty String');
+}
+
+checkinput(sayHi, 'test', 'asdasd', 'adasd', 'xx', 'asdasdasd');
