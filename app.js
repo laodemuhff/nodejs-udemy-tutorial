a7ec3f5016eef649e2087901be16a3e401ff1bb3
Farhan Fauzan AVO <farhan@AVOs-MacBook-Pro.local>
2021-09-13 14:02:08 +0700

    spread operator

--- app.js
+++ app.js
@@ -1,105 +1,146 @@
-const startGameBtn = document.getElementById('start-game-btn');
+// foreach
+// bedanya dengan for of loop, for each bisa retrieve index
+// const prices = [10,11,15,13];
+// const tax = 0.1;
+// const adjustedPrice = [];
 
-const ROCK = 'ROCK';
-const PAPER = 'PAPER';
-const SCISSORS = 'SCISSORS';
-const DEFAULT_USER_CHOICE = ROCK;
-const RESULT_DRAW = 'DRAW';
-const RESULT_PLAYER_WINS = 'PLAYER_WINS';
-const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+// prices.forEach((price, idx, prices) => {
+//   const priceObj = {
+//     index : idx, 
+//     adjustedPrice : price + (price * tax)
+//   }
 
-let gameIsRunning = false;
+//   adjustedPrice.push(priceObj)
+// });
 
-const getPlayerChoice = () => {
-  const selection = prompt(
-    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
-    ''
-  ).toUpperCase();
-  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
-    return;
-  }
-  return selection;
-};
+// console.log(adjustedPrice);
 
-const getComputerChoice = () => {
-  const randomValue = Math.random();
-  if (randomValue < 0.34) {
-    return ROCK;
-  } else if (randomValue < 0.67) {
-    return PAPER;
-  } else {
-    return SCISSORS;
-  }
-};
 
-const getWinner = (
-  cChoice,
-  pChoice = cCHoice === 'ROCK' ? PAPER : DEFAULT_USER_CHOICE
-) =>
-  cChoice === pChoice
-    ? RESULT_DRAW
-    : (cChoice === ROCK && pChoice === PAPER) ||
-      (cChoice === PAPER && pChoice === SCISSORS) ||
-      (cChoice === SCISSORS && pChoice === ROCK)
-    ? RESULT_PLAYER_WINS
-    : RESULT_COMPUTER_WINS;
+// const adjustedPrice2 = prices.map((price, idx, prices) => {
+//   return {
+//     index : idx,
+//     adjustedPrice : price + (price * tax)
+//   }
+// });
 
-// if (cChoice === pChoice) {
-//   return RESULT_DRAW;
-// } else if (
-//   (cChoice === ROCK && pChoice === PAPER) ||
-//   (cChoice === PAPER && pChoice === SCISSORS) ||
-//   (cChoice === SCISSORS && pChoice === ROCK)
-// ) {
-//   return RESULT_PLAYER_WINS;
-// } else {
-//   return RESULT_COMPUTER_WINS;
-// }
+// console.log(prices, adjustedPrice2);
 
-startGameBtn.addEventListener('click', () => {
-  if (gameIsRunning) {
-    return;
-  }
-  gameIsRunning = true;
-  console.log('Game is starting...');
-  const playerChoice = getPlayerChoice();
-  const computerChoice = getComputerChoice();
-  let winner;
-  if (playerChoice) {
-    winner = getWinner(computerChoice, playerChoice);
-  } else {
-    winner = getWinner(computerChoice);
-  }
-  let message = `You picked ${playerChoice ||
-    DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefore you `;
-  if (winner === RESULT_DRAW) {
-    message = message + 'had a draw.';
-  } else if (winner === RESULT_PLAYER_WINS) {
-    message = message + 'won.';
-  } else {
-    message = message + 'lost.';
-  }
-  alert(message);
-  gameIsRunning = false;
+// const prices2 = [10, 20, 5, 23, 98, 8]
+// const sortedPrice2 = prices2.sort();
+// //price2.sort() do works!
+
+// console.log(sortedPrice2, prices2);
+
+// // ascending
+// prices2.sort((a,b) => {
+//    if(a > b){
+//      return 1;
+//    }else if(a === b){
+//      return 0;
+//    }else{
+//      return -1;
+//    }
+// });
+
+// console.log(prices2);
+
+// prices2.reverse();
+
+// console.log(prices2);
+
+
+// const filteredPrices2 = prices2.filter(price => price >= 20)
+
+// console.log(filteredPrices2, prices2);
+
+// const prices3 = [11,13,8,7,23]
+// prices3.reverse()
+// console.log(prices3)
+
+
+// const price = [11,23,11,23,11]
+
+// const sum = price.reduce((prevValue, curValue, idx, prices)=>{
+//     return prevValue + curValue;
+// }, 0)
+
+// console.log(sum);
+
+// const products = [
+//   {
+//     price : 12,
+//     name : 'skincare'
+//   },
+//   {
+//     price : 12,
+//     name : 'sabun muka'
+//   }
+// ];
+
+// const transformedArray = products.map(obj => obj.price);
+// const sum = transformedArray.reduce((prevValue, curValue) => prevValue + curValue, 0)
+// const sum = products.reduce((prevValue, curValue) => prevValue + curValue.price, 0)
+// const sum = products.map(obj => obj.price).reduce((prevValue, curValue) => prevValue + curValue,0)
+
+// console.log(sum);
+
+
+// const data = ['Farhan', 'Fauzan']
+// console.log(data.join(' '));
+
+// const data = "Jakarta;10,98;11:00 AM"
+// console.log(data.split(';'))
+//output : ['Jakarta', '10,98', '11:00 AM']
+
+
+
+// spread operator
+
+// const data = ["Farhan", "Fauzan", "S.kom"]
+// // const copiedData = ...data // error
+// const copiedData = [...data]
+// data.push("M.kom");
+// console.log(data, copiedData);
+
+
+// const numbers = [2,5,12,4];
+// // console.log(Math.min(numbers)); // error
+// console.log(Math.min(...numbers));
+
+// be careful with objects !
+const products = [
+  {
+    price : 100,
+    name : "skincare",
+    sizes : [
+      'Small',
+      'Medium'
+    ]
+  },
+
+  {
+    price : 200,
+    name : "sabun muka",
+    sizes : [
+      "Small"
+    ]
+  },
+];
+
+const copiedProducts = [...products]
+copiedProducts.push({
+  price : 300,
+  name : "sabun cuci muka"
 });
+copiedProducts[0].price = 1000
+console.log(products, copiedProducts);
 
-
-const sum = (...data) => {
-  const isValidate = (data) => {
-     return isNaN(data) ? 0 : data
-  }
-
-  if(isValidate()){
-    let total = 0;
-    for(const nilai of data){
-        total += nilai;
-    }
-
-    return total;
-  }
-
-  alert('Invalid Request 404');
-}
-
-console.log(sum(1,2,3,4));
+// solution : map
+const copiedProducts2 = [...products].map(product => ({
+  price : product.price,
+  name : product.name,
+  sizes : [...product.sizes]
+}))
+copiedProducts2[0].price = 800;
+copiedProducts2[0].sizes[0] = 'SM'
+console.log(copiedProducts2, products);
